bf8531e1ce944cc46bfad8c343e313099a2dbc0c
Michael Howitz <mh@gocept.com>
2021-02-10 07:51:22 +0100

    Back to development: 3.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(*rnames):
 
 
 setup(name='z3c.evalexception',
-      version='3.0',
+      version='3.1.dev0',
       license='ZPL 2.1',
       description="Debugging middlewares for zope.publisher-based web "
       "applications",
